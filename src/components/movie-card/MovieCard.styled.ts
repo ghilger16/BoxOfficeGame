bd90b639c1f4b22e36bbb165b98e8811db2ea95e
ghilger <ghilger16@yahoo.com>
2024-02-22 15:53:30 -0700

    updates to movie card

--- src/components/movie-card/MovieCard.styled.ts
+++ src/components/movie-card/MovieCard.styled.ts
@@ -1,9 +1,16 @@
 import styled from "styled-components";
 
-export const MovieCardContainer = styled.div``;
+export const MovieCardContainer = styled.div`
+  border: 1px solid black;
+  display: flex;
+  flex-direct: row;
+  padding: 5px;
+`;
 
 export const MovieImage = styled.img``;
 
-export const MovieDetailsSection = styled.div``;
+export const MovieDetailsSection = styled.div`
+  margin-left: 5px;
+`;
 
 export const Title = styled.div``;
